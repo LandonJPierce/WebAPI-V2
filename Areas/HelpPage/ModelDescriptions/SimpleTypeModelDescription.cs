c81aa13f91473fd0397d7d93eb931986e698d25a
LandonJPierce <landers4000@gmail.com>
2022-11-30 02:34:50 -0600

    Add project files.

---
+++ Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace WebAPI_V2.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebAPI_V2.WebApiApplication" Language="C#" %>
